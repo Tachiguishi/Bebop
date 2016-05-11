ef0d23914244bed6aef52512a4cae10c112be6f0
TachiTukimi <TachiguishiRor@gmail.com>
2016-05-11 15:03:06 +0800

    Algorithmic covering segments

--- Algorithmic/covering_segments/covering_segments.py
+++ Algorithmic/covering_segments/covering_segments.py
@@ -7,16 +7,33 @@ Segment = namedtuple('Segment', 'start end')
 def optimal_points(segments):
     points = []
     #write your code here
+    segments.sort(segmentComp)
     for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+        if len(points) == 0:
+            points.append(s.end)
+            continue
+        if s.start > points[len(points)-1]:
+            points.append(s.end)
+            continue
+        if s.end < points[len(points)-1]:
+            points[len(points)-1] = s.end
+
     return points
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *data = map(int, input.split())
-    segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
-    points = optimal_points(segments)
-    print(len(points))
-    for p in points:
-        print(p, end=' ')
+def segmentComp(a,b):
+    if a.start < b.start:
+        return -1
+    if a.start == b.start:
+        return 0
+    if a.start > b.start:
+        return 1
+
+inputData = raw_input()
+data = map(int, inputData.split())
+n = data.pop(0)
+segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+print(segments)
+print(segments[0].start)
+points = optimal_points(segments)
+print(len(points))
+print(points)

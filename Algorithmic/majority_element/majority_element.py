b540951678a6efb4bd77f36f7dd5015e78e1aaa8
TachiTukimi <TachiguishiRor@gmail.com>
2016-09-07 02:00:37 +0800

    Algorithmic majorityElement.py

--- Algorithmic/majority_element/majority_element.py
+++ Algorithmic/majority_element/majority_element.py
@@ -1,18 +1,40 @@
-# Uses python3
-import sys
+# Uses python2
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
+def election(candidates):
+    #if type(candidates) != 'list':
+    #    return;
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
-        print(1)
+    winner = candidates[0]
+    vote   = 0
+
+    for candidate in candidates:
+        if winner == candidate:
+            vote += 1
+        else:
+            vote -= 1
+        if 0 == vote:
+            winner = candidate
+            vote = 1
+
+    return winner
+
+def isMajority(candidates, winner):
+    counter = 0
+    for candidate in candidates:
+        if winner == candidate:
+            counter += 1
+    if counter > len(candidates)/2.0:
+        return True
     else:
-        print(0)
+        return False
+
+def get_majority_element(a):
+    candidate = election(a)
+    if isMajority(a, candidate):
+        print candidate
+    else:
+        print 0
+
+input = raw_input();
+a = list(map(int, input.split()))
+get_majority_element(a)
